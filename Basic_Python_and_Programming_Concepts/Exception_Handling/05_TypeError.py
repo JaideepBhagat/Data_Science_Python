c35a1d5a09103b401128b47a35afb4b9dc87c00e
Jaideep <jaideep.bhagat@yahoo.in>
2024-08-26 11:06:26 +0530

    Exception Handling simple examples

--- Basic_Python_and_Programming_Concepts/Exception_Handling/05_TypeError.py
+++ Basic_Python_and_Programming_Concepts/Exception_Handling/05_TypeError.py
@@ -1,7 +1,7 @@
 try:
-    num1 = input("Number 1:")
-    num2 = input("Number 2:")
+    num1 = input("Number 1: ")
+    num2 = input("Number 2: ")
     div = num1/num2
     print(div)
-except TypeError:
+except TypeError as e:
     print("Error: cannot process arithmetic operations on string data")
