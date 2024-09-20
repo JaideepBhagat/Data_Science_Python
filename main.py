ac910d486e8c158953adbcd1503df1634edb2dbb
Jaideep <jaideep.bhagat@yahoo.in>
2024-09-20 12:03:46 +0530

    Numpy and Pandas

--- main.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df1 = pd.DataFrame({'Name': ['Alice', 'Bob'], 'Age': [25, 30]})
-df2 = pd.DataFrame({'Name': ['Charlie', 'David'], 'Age': [35, 40]})
-
-result = pd.concat([df1, df2], ignore_index=True)
-print(result)
