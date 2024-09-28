15ac42d293adb6c9f1f8578b79b973d601a37373
Jaideep <jaideep.bhagat@yahoo.in>
2024-09-28 12:12:57 +0530

    Basic Algebra and calculus

--- Mathematics_and_Statistics/Basic_algebra_and_calculus/01_Linear_Equation.py
+++ Mathematics_and_Statistics/Basic_algebra_and_calculus/01_Linear_Equation.py
@@ -1,12 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.linspace(-5, 5, 1000)
+# Define the linear equation: 2x + 3y = 5
+x = np.linspace(-5, 5, 100)
 y = (5 - 2*x) / 3
 
 plt.plot(x, y, label="2x + 3y = 5")
-plt.axhline(0, color='black',linewidth=0.25)
-plt.axvline(0, color='black',linewidth=0.25)
+plt.axhline(0, color='black',linewidth=1)
+plt.axvline(0, color='black',linewidth=1)
 plt.grid(True)
 plt.legend()
 plt.show()
