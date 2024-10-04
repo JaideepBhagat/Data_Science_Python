69e81ea2b279fb35dbd58773352d847331fa5ca7
Jaideep <jaideep.bhagat@yahoo.in>
2024-10-04 11:53:22 +0530

    Probability Distributions

--- Mathematics_and_Statistics/05_Probability_Distributions/03_Poisson_Distribution.py
+++ Mathematics_and_Statistics/05_Probability_Distributions/03_Poisson_Distribution.py
@@ -4,10 +4,11 @@ from scipy.stats import poisson
 
 # Poisson distribution with λ = 3
 λ = 3  # rate parameter
+data = np.random.poisson(λ, 100)
 
 # Plot a bar plot
-x = np.arange(0, 15)
+x = np.arange(0, 100)
 y = poisson.pmf(x, λ)
-plt.bar(x, y, color='lightblue')
+plt.bar(x, data, color='lightblue')
 plt.title('Poisson Distribution')
 plt.show()
