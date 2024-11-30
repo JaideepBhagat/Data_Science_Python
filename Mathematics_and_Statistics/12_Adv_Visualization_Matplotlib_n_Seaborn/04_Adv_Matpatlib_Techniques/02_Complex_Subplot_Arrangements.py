4b1ce8bd7e51af6b51f2c3bae40ce685e4ba33d7
jaideep <jaideep.bhagat@yahoo.in>
2024-11-30 13:12:58 +0530

    DataVisualization

--- Mathematics_and_Statistics/12_Adv_Visualization_Matplotlib_n_Seaborn/04_Adv_Matpatlib_Techniques/02_Complex_Subplot_Arrangements.py
+++ Mathematics_and_Statistics/12_Adv_Visualization_Matplotlib_n_Seaborn/04_Adv_Matpatlib_Techniques/02_Complex_Subplot_Arrangements.py
@@ -1,25 +1,25 @@
 import matplotlib.pyplot as plt
-import numpy as np
+import seaborn as sns
 import kagglehub
 import os
 import pandas as pd
 # Download sales data
-path = kagglehub.dataset_download("rohitsahoo/sales-forecasting")
+path = kagglehub.dataset_download("sakshisatre/tips-dataset")
+tips = pd.read_csv(os.path.join(path, "tip.csv"))
 print(path)
-sales = pd.read_csv(os.path.join(path, "train.csv"))
 # Create subplots
 fig, axs = plt.subplots(2, 2, figsize=(10, 8))
 # Plot the sales data in the first subplot
-axs[0, 0].plot(sales.pivot_table(index='Region', columns='Category', values='Sales', aggfunc='mean'))
-axs[0, 0].set_title('Sales Data')
+sns.boxplot(x='sex', y='tip', data=tips, ax=axs[0, 0], palette='light:lightblue', hue='smoker')
+axs[0, 0].set_title('Tip by Gender')
 # Plot the bar chart in the second subplot
-axs[0, 1].bar(sales['Category'], sales['Sales'])
-axs[0, 1].set_title('Sales by Category')
+sns.violinplot(x='smoker', y='total_bill', data=tips, ax=axs[0, 1], palette='light:lightgreen', hue='sex')
+axs[0, 1].set_title('Bill by Smokers')
 # Plot the scatter plot in the third subplot
-axs[1, 0].scatter(sales['Region'], sales['Sales'])
+sns.barplot(x='day', y='total_bill', data=tips, ci='sd', ax=axs[1, 0], palette= 'pastel')
 axs[1, 0].set_title('Sales by Region')
 # Plot the histogram in the fourth subplot
-axs[1, 1].hist(sales['Sales'], bins=10)
+sns.kdeplot(tips['total_bill'], ax=axs[1, 1], fill=True, color='g')
 axs[1, 1].set_title('Sales Distribution')
 # Adjust spacing between subplots
 plt.tight_layout()
